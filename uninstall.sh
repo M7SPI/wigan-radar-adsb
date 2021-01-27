4f9351709678fa39b18efcbeb51322b1cb74cffe
simonedegiacomi <simonedegiacomi97@gmail.com>
2021-01-27 21:46:16 +0100

    rinomina file nello script di disinstallazione

--- uninstall.sh
+++ uninstall.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -x
 
-IPATH=/usr/local/share/adsbexchange
+IPATH=/usr/local/share/flyitalyadsb
 
-systemctl disable --now adsbexchange-mlat
-systemctl disable --now adsbexchange-feed
+systemctl disable --now flyitalyadsb-mlat
+systemctl disable --now flyitalyadsb-feed
 
-rm -f /lib/systemd/system/adsbexchange-mlat.service
-rm -f /lib/systemd/system/adsbexchange-feed.service
+rm -f /lib/systemd/system/flyitalyadsb-mlat.service
+rm -f /lib/systemd/system/flyitalyadsb-feed.service
 
 rm -rf "$IPATH"
 
 set +x
 
 echo -----
-echo "adsbexchange feed scripts have been uninstalled!"
+echo "flyitalyadsb feed scripts have been uninstalled!"
