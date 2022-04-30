564d65841fe2f81117b28e4a6a3b5e3590af3a97
flyitalyadsb <admin@flyitalyadsb.com>
2022-04-30 17:07:17 +0200

    Big Update
    - Install updater always
    - Ask for newsletter
    - Ask for interface (tar1090)
    - Better translation

--- setup.sh
+++ setup.sh
@@ -1,5 +1,30 @@
 #!/bin/bash -eu
-
+#####################################################################################
+#                        FLY ITALY ADSB SETUP SCRIPT FORKED                         #
+#####################################################################################
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                                   #
+# Copyright (c) 2022 Fly Italy Adsb                                                 #
+#                                                                                   #
+# Permission is hereby granted, free of charge, to any person obtaining a copy      #
+# of this software and associated documentation files (the "Software"), to deal     #
+# in the Software without restriction, including without limitation the rights      #
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell         #
+# copies of the Software, and to permit persons to whom the Software is             #
+# furnished to do so, subject to the following conditions:                          #
+#                                                                                   #
+# The above copyright notice and this permission notice shall be included in all    #
+# copies or substantial portions of the Software.                                   #
+#                                                                                   #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR        #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,          #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE       #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER            #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,     #
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE     #
+# SOFTWARE.                                                                         #
+#                                                                                   #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #####################################################################################
 #                        ADS-B EXCHANGE SETUP SCRIPT FORKED                         #
 #####################################################################################
@@ -57,7 +82,7 @@ LOGDIRECTORY="$PWD/logs"
 
 BACKTITLETEXT="Script di installazione del feed di Fly Italy Adsb"
 
-whiptail --backtitle "$BACKTITLETEXT" --title "$BACKTITLETEXT" --yes-button SI --no-button NO --yesno "Grazie per aver scelto di condividere i tuoi dati con Fly Italy Adsb.\n\nFlyItalyAdsb.com è la prima community italiana che tratta di ADS-B. Questo script configurerà in automatico il tuo ricevitore per condividere i dati con Fly Italy Adsb\n\nVuoi continuare con l'installazione?" 13 78
+whiptail --backtitle "$BACKTITLETEXT" --title "$BACKTITLETEXT" --yes-button SI --no-button NO --yesno "Grazie per aver scelto di condividere i tuoi dati con Fly Italy Adsb.\n\nFly Italy Adsb è la prima community italiana che tratta di ADS-B. Questo script configurerà in automatico il tuo ricevitore per condividere i dati con Fly Italy Adsb\n\nVuoi continuare con l'installazione?" 13 78
 if [[ $? != 0 ]]; then abort; fi
 
 FLYITALYADSB_=$(whiptail --backtitle "$BACKTITLETEXT" --title "Nome utente" --nocancel --inputbox "\nPer favore inserisci un nome per il tuo ricevitore (http://flyitalyadsb.com/stato-mlat)\n\nUsa solo lettere e numeri.\nEsempio: \"Giorgio34\", \"Piacenza1\", etc." 12 78 3>&1 1>&2 2>&3)
@@ -248,7 +273,7 @@ fi
     echo 70
     sleep 0.25
 
-    # SETUP FEEDER TO SEND DUMP1090 DATA TO ADS-B EXCHANGE
+    # SETUP FEEDER TO SEND DUMP1090 DATA TO FLY ITALY ADSB
 
     echo "" >> $LOGFILE
     echo " COMPILANDO E INSTALLANDO IL FEED" >> $LOGFILE
@@ -301,15 +326,11 @@ fi
     tee /etc/default/flyitalyadsb > /dev/null 2>> $LOGFILE <<EOF
     INPUT="127.0.0.1:30005"
     REDUCE_INTERVAL="0.5"
-
     # nome utente per controllare lo stato della MLAT-Multilaterazione (flyitalyadsb.com/stato-mlat)
     UTENTE="${NOSPACENAME}"
-
     LATITUDINE="$RECEIVERLATITUDE"
     LONGITUDINE="$RECEIVERLONGITUDE"
-
     ALTITUDINE="$RECEIVERALTITUDE"
-
     RISULTATI="--results beast,connect,localhost:30104"
     RISULTATI2="--results basestation,listen,31003"
     RISULTATI3="--results beast,listen,30157"
@@ -344,25 +365,116 @@ EOF
     echo 100
     sleep 0.25
 
-    cp $LOGFILE $IPATH/lastlog &>/dev/null
+    MLAT_GIT_DIR="/usr/local/share/flyitalyadsb/mlat-client-git"
+
+    UPDATER_SYSTEMD_SERVICE_PATH="/lib/systemd/system/flyitalyadsb-mlat-updater.service"
+    read -r -d '' UPDATER_SYSTEMD_SERVICE <<-'EOF'
+    [Unit]
+    Description=Keep mlat-client up to date
+    After=network.target
+
+    [Service]
+    Type=simple
+    User=root
+    WorkingDirectory=/usr/local/share/flyitalyadsb/mlat-client-git
+    ExecStart=/usr/local/share/flyitalyadsb/mlat-client-git/scripts/update.sh
+
+    [Install]
+    WantedBy=timers.target
+
+EOF
+
+    ###
+
+    UPDATER_SYSTEMD_TIMER_PATH="/lib/systemd/system/flyitalyadsb-mlat-updater.timer"
+    read -r -d '' UPDATER_SYSTEMD_TIMER <<-'EOF'
+    [Unit]
+    Description=Keep mlat-client up to date
+
+    [Timer]
+    OnCalendar=daily
+    Persistent=true
+
+    [Install]
+    WantedBy=timers.target
+
+EOF
+
+
+    if [ ! -d "$MLAT_GIT_DIR/.git" ]; then
+        echo "Scarico il repository di mlat-client" >> $LOGFILE 2>&1
+        mkdir -p "$MLAT_GIT_DIR"
+        chown flyitalyadsb "$MLAT_GIT_DIR"
+        sudo -u flyitalyadsb git clone "$MLAT_REPO" "$MLAT_GIT_DIR"  >> $LOGFILE 2>&1
+
+        echo "Eseguo lo script di aggiornamento" >> $LOGFILE 2>&1
+        cd "$MLAT_GIT_DIR" || exit 1
+        ./scripts/update.sh >> $LOGFILE 2>&1
+    fi
+
+    echo "Installo il timer systemd per scaricare in automatico gli aggiornamenti" >> $LOGFILE 2>&1
+    printf "%s" "$UPDATER_SYSTEMD_SERVICE" > "$UPDATER_SYSTEMD_SERVICE_PATH"
+    chmod 644 "$UPDATER_SYSTEMD_SERVICE_PATH"
+    printf "%s" "$UPDATER_SYSTEMD_TIMER" > "$UPDATER_SYSTEMD_TIMER_PATH"
+    chmod 644 "$UPDATER_SYSTEMD_TIMER_PATH"
+
+    echo "Avvio updater" >> $LOGFILE 2>&1
+    systemctl enable flyitalyadsb-mlat-updater.timer >> $LOGFILE 2>&1
+    systemctl start flyitalyadsb-mlat-updater.timer >> $LOGFILE 2>&1
+
+    echo "Riavvio mlat-client" >> $LOGFILE 2>&1
+    systemctl restart flyitalyadsb-mlat >> $LOGFILE 2>&1
+
+    echo "Updater installato correttamente" >> $LOGFILE 2>&1
 
 } | whiptail --backtitle "$BACKTITLETEXT" --title "Impostando il feed di Fly Italy Adsb"  --gauge "\nImpostando il feed di Fly Italy Adsb.\nPotrebbe impiegarci qualche minuto..." 8 60 0
+
+whiptail --backtitle "$BACKTITLETEXT" --title "$BACKTITLETEXT" --yes-button SI --no-button NO --yesno "Vuoi installare una mappa in cui sono visibili gli aerei che \nstai ricevendo in questo momento?\nSarà accessibile a tuo_ip/flyitalyadsb" 9 70 0 
+INTERFACCIA=$?
+if [ $INTERFACCIA = 0 ]; then
+{   
+    set -e
+    trap 'echo "------------"; echo "[ERROR] Error in line $LINENO when executing: $BASH_COMMAND"' ERR
+
+    function getGIT() {
+        # getGIT $REPO $BRANCH $TARGET-DIR
+        if [[ -z "$1" ]] || [[ -z "$2" ]] || [[ -z "$3" ]]; then
+            echo "getGIT wrong usage, check your script or tell the author!" 1>&2
+            return 1
+        fi
+        if ! cd "$3" &>/dev/null || ! git fetch origin "$2" >> $LOGFILE 2>&1|| ! git reset --hard FETCH_HEAD>> $LOGFILE 2>&1; then
+            if ! rm -rf "$3" || ! git clone --depth 2 --single-branch --branch "$2" "$1" "$3" >> $LOGFILE 2>&1; then
+                return 1
+            fi
+        fi
+        return 0
+    }
+    REPO="https://github.com/wiedehopf/tar1090"
+    BRANCH="master"
+    GIT="/usr/local/share/tar1090/git"
+    getGIT "$REPO" "$BRANCH" "$GIT" 
+    bash "$GIT/install.sh" "/run/flyitalyadsb-feed" "flyitalyadsb"
+}| whiptail --backtitle "$BACKTITLETEXT" --title "Impostando l'interfaccia di Fly Italy Adsb"  --gauge "\nImpostando l'interfaccia di Fly Italy Adsb.\nPotrebbe impiegarci qualche minuto..." 8 60 0
+fi
+
+whiptail --backtitle "$BACKTITLETEXT" --title "$BACKTITLETEXT" --yes-button SI --no-button NO --yesno "Se lo desideri, lascia una email per essere contattato nel caso il tuo ricevitore dovesse andare offline per più di 3 giorni\ne per iscriverti alla nostra newsletter (Non più di una email al mese)." 10 70  
+MAIL=$?
+if [ $MAIL = 0 ]; then
+    email=$(whiptail --backtitle "$BACKTITLETEXT" --title "Email"  --inputbox "\nInserisci la tua email" 8 60 3>&1 1>&2 2>&3)
+    curl -d "mail=$email" https://flyitalyadsb.com/newsletter.php 
+fi
+
 
 ## SETUP COMPLETE
 
 ENDTEXT="
-L'installazione è stata completata.
-
-Ora stai condividendo i tuoi dati con FLy Italy Adsb.
-
-Grazie ancora per condividere i tuoi dati con Fly Italy Adsb!
-
-Se sei curioso e hai abilitato la funzione MLAT, allora puoi controllare lo stato del tuo ricevitore a questo indirizzo:
-https://www.flyitalyadsb.com/stato-mlat
-
+L'installazione è stata completata. Ora stai condividendo i tuoi dati con Fly Italy Adsb.
+Puoi controllare lo stato del tuo ricevitore a questa pagina:
+---------------------
+https://www.flyitalyadsb.com/stato-mlat/${FLYITALYADSB_}
+---------------------
 Se hai incontrato qualche problema nell'installazione del feed o vorresti darci qualche suggerimento visita:
 https://www.flyitalyadsb.com
-
 oppure mandaci direttamente una email all'indirizzo:
 mailto:installazione@flyitalyadsb.com
 "
@@ -373,37 +485,38 @@ if ! nc -z 127.0.0.1 30005 && command -v nc &>/dev/null; then
 ---------------------
 Nessun dato disponibile sulla porta 30005!
 ---------------------
-Se il feed deve ricevere i dati da un altra porta/sorgente vedi questa pagina per modificare le impostazioni:
-
+Se non hai installato nessun decoder visita questa pagina:
+https://www.flyitalyadsb.com/come-costruire-un-ricevitore-ads-b/#installazione-dump1090-fa
+Se invece il feed deve ricevere i dati da un altra porta/ip vai a questo link:
 https://flyitalyadsb.com/configurazione-script
 --------------------
 "
     if [ -f /etc/fr24feed.ini ] || [ -f /etc/rb24.ini ]; then
         ENDTEXT2+="
 Sembra che stai usando FR24 o BR24
-Questo significa che devi usare un altro decoder per far si che i dati siano disponibili sulla porta 30005!
-
-Se hai una chiavetta SDR connessa a questo dispositivo, ti consigliamo di installare questo decoder:
-
-https://github.com/wiedehopf/adsb-scripts/wiki/readsb-script
+Questo significa che devi abilitare la trasmissione dei dati sulla porta 30005! Per farlo:
+- vai alla pagina: ip_del_tuo_raspberry:8754/settings.
+- in \"process arguments\" aggiungi \" --net\" 
+- imposta NO in RAW DATA, SBS FEED e DECODED DATA.
+--Premi \"Save\" e poi \"restart\" 
+Se non ti dovesse ancora funzionare, scrivici a mailto:installazione@flyitalyadsb.com
 ---------------------
 "
     else
         ENDTEXT2+="
 Se hai connesso a questo dispositivo una chiavetta SDR ma non hai 
-ancora installato il decoder ti consigliamo questo:
-
-https://github.com/wiedehopf/adsb-scripts/wiki/readsb-script
+ancora installato il decoder visita questa pagina:
+https://www.flyitalyadsb.com/come-costruire-un-ricevitore-ads-b/#installazione-dump1090-fa
 ---------------------
 "
     fi
-    whiptail --title "Script di installazione del feed di Fly Italy Adsb" --msgbox "$ENDTEXT2" 24 73
+    whiptail --title "Script di installazione del feed di Fly Italy Adsb" --msgbox "$ENDTEXT2" 27 73
     echo -e "$ENDTEXT2"
 else
     # Display the thank you message box.
-    whiptail --title "Script di installazione del feed di Fly Italy Adsb" --msgbox "$ENDTEXT" 24 73
+    whiptail --title "Script di installazione del feed di Fly Italy Adsb" --msgbox "$ENDTEXT" 27 73
     echo -e "$ENDTEXT"
 fi
 
-
+cp $LOGFILE $IPATH/lastlog &>/dev/null
 exit 0
