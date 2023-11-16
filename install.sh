549447b1dbe7323d479130cadf53ca8631a75568
flyitalyadsb <info@flyitalyadsb.com>
2023-11-16 23:12:52 +0100

    bug fixed

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ git clone https://github.com/flyitalyadsb/fly-italy-adsb.git "$TMP"
 cd "$TMP" || exit 1;
 BACKTITLETEXT="Fly Italy Adsb"
 if whiptail --backtitle "$BACKTITLETEXT" --title "$BACKTITLETEXT" --yes-button ENGLISH --no-button ITALIANO --yesno "Benvenuto su Fly Italy Adsb. Welcome to Fly Italy Adsb\n\nSeleziona la tua lingua. Select yor language. " 13 78;
-then $PWD/english/setup.sh
-else $PWD/italian/setup.sh
+then source $PWD/english/setup.sh
+else source $PWD/italian/setup.sh
 fi
 
