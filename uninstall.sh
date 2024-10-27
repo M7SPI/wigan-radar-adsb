68602bc72518177bf7e0126a481793551511dc65
M7SPI <stuparr1@hotmail.co.uk>
2024-10-27 13:33:42 +0000

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -x
 
-IPATH=/usr/local/share/flyitalyadsb
+IPATH=/usr/local/share/wiganradaradsb
 
-systemctl disable --now flyitalyadsb-mlat
-systemctl disable --now flyitalyadsb-feed
+systemctl disable --now wiganradaradsb-mlat
+systemctl disable --now wiganradaradsb-feed
 
-rm -f /lib/systemd/system/flyitalyadsb-mlat.service
-rm -f /lib/systemd/system/flyitalyadsb-feed.service
+rm -f /lib/systemd/system/wiganradaradsb-mlat.service
+rm -f /lib/systemd/system/wiganradaradsb-feed.service
 
 rm -rf "$IPATH"
 
 set +x
 
 echo -----
-echo "flyitalyadsb feed scripts have been uninstalled!"
+echo "wiganradaradsb feed scripts have been uninstalled!"
