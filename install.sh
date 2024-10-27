7577b45881578c7c50c4c4fd81bd3dfb17308416
M7SPI <stuparr1@hotmail.co.uk>
2024-10-27 13:02:17 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ if ! command -v git; then
     apt-get install -y git
 fi
 rm -rf "$TMP"
-git clone https://github.com/flyitalyadsb/fly-italy-adsb.git "$TMP"
+git clone https://github.com/M7SPI/wigan-radar-adsb.git "$TMP"
 cd "$TMP" || exit 1;
-BACKTITLETEXT="Fly Italy Adsb"
+BACKTITLETEXT="Wigan Radar Adsb"
 if whiptail --backtitle "$BACKTITLETEXT" --title "$BACKTITLETEXT" --yes-button ENGLISH --no-button ITALIANO --yesno "Wigan Radar Adsb. Welcome to Wigan Radar Adsb\n\nSeleziona la tua lingua. Select yor language. " 13 78;
 then source $PWD/english/setup.sh
 else source $PWD/italian/setup.sh
